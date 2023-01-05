b7ff124e4049170fd7e34c8bd192cccc7d5f6307
romeby <roman.zhmailik@gmail.com>
2023-01-05 18:38:16 +0300

    05.01

--- src/main/java/com/roman/task2/main/FilmMain.java
+++ src/main/java/com/roman/task2/main/FilmMain.java
@@ -16,14 +16,7 @@ import java.util.List;
 public class FilmMain {
     static Logger logger = LogManager.getLogger();
     public static void main(String[] args) {
-        List<FilmStudio> workerList = new ArrayList<>(7);
-        workerList.add(FilmStudio.STAGE_DIRECTOR);
-        workerList.add(FilmStudio.SCENARIO);
-        workerList.add(FilmStudio.MUSICIAN);
-        workerList.add(FilmStudio.ACTORS);
-        workerList.add(FilmStudio.ADVERTISMENT);
-        workerList.add(FilmStudio.MONTAGE);
-        workerList.add(FilmStudio.EQUIPMENT);
+
         Order order = new Order( "Spilberg" , "Pulp fiction", 0);
         Order order1 = new Order( "Spilberg" , "Pu", -1);
         Order order2 = new Order("Vanstein", "Titanic movie. Full version",1);
