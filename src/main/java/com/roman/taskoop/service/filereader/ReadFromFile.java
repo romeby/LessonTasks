f89046d311d8960b5a0d6e8f40ef0ba888ea8aae
romeby <roman.zhmailik@gmail.com>
2023-01-26 01:45:49 +0300

    26.01

--- src/main/java/com/roman/taskoop/service/filereader/ReadFromFile.java
+++ src/main/java/com/roman/taskoop/service/filereader/ReadFromFile.java
@@ -1,10 +1,13 @@
 package com.roman.taskoop.service.filereader;
 
+import com.roman.taskoop.entity.FloorCareAppliance;
+import com.roman.taskoop.entity.KitchenAppliance;
 import com.roman.taskoop.entity.LaundryAppliance;
+import com.roman.taskoop.entity.TvAndEntertainment;
 
 public interface ReadFromFile {
-    LaundryAppliance floorReader(String filename);
-    LaundryAppliance kitchenReader(String filename);
+    FloorCareAppliance floorReader(String filename);
+    KitchenAppliance kitchenReader(String filename);
     LaundryAppliance laundryReader(String filename);
-    LaundryAppliance entertainmentReader(String filename);
+    TvAndEntertainment entertainmentReader(String filename);
 }
--- src/main/java/com/roman/taskoop/service/filereader/impl/ReadFromFileImpl.java
+++ src/main/java/com/roman/taskoop/service/filereader/impl/ReadFromFileImpl.java
@@ -1,19 +1,22 @@
 package com.roman.taskoop.service.filereader.impl;
 
+import com.roman.taskoop.entity.FloorCareAppliance;
+import com.roman.taskoop.entity.KitchenAppliance;
 import com.roman.taskoop.entity.LaundryAppliance;
+import com.roman.taskoop.entity.TvAndEntertainment;
 import com.roman.taskoop.service.filereader.ReadFromFile;
 
 public class ReadFromFileImpl implements ReadFromFile {
-    private static final String FILE_NAME = "//data/InputFile.txt";
+    private static final String FILE_NAME = "data\\InputFile.txt";
     private static final String SEPARATOR = "\\s+";
 
     @Override
-    public LaundryAppliance floorReader(String filename) {
+    public FloorCareAppliance floorReader(String filename) {
         return null;
     }
 
     @Override
-    public LaundryAppliance kitchenReader(String filename) {
+    public KitchenAppliance kitchenReader(String filename) {
         return null;
     }
 
@@ -23,7 +26,7 @@ public class ReadFromFileImpl implements ReadFromFile {
     }
 
     @Override
-    public LaundryAppliance entertainmentReader(String filename) {
+    public TvAndEntertainment entertainmentReader(String filename) {
         return null;
     }
 }
