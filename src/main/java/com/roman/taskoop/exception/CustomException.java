e73b1b47554f04a1d80bc3a8ccdeeac849b4d9c3
romeby <roman.zhmailik@gmail.com>
2023-01-26 19:06:19 +0300

    26.01

--- src/main/java/com/roman/taskoop/exception/CustomException.java
+++ src/main/java/com/roman/taskoop/exception/CustomException.java
@@ -6,7 +6,6 @@ public class CustomException extends Exception{
 
     public CustomException(String message) {
         super(message);
-        super.getStackTrace();
     }
 
     public CustomException(String message, Throwable cause) {
