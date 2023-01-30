4dc7d8fb80164fb710d27a7af02c15cbf77829d3
romeby <roman.zhmailik@gmail.com>
2023-01-30 19:07:45 +0300

    30.01

--- src/main/java/com/roman/taskoop/validator/StringValidator.java
+++ src/main/java/com/roman/taskoop/validator/StringValidator.java
@@ -1,7 +1,7 @@
 package com.roman.taskoop.validator;
 
 public interface StringValidator {
-    boolean validateString (String stringString);
+    boolean validateString (String typeString);
     boolean validateInt (String intString);
     boolean validateDouble (String doubleString);
     boolean validateBoolean (String booleanString);
